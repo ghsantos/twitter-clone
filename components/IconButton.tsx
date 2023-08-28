7cd5d2985e8c331b91959f6e9cf0a3afd7906f62
ghsantos <ghsantos933@gmail.com>
2023-08-28 19:46:28 -0300

    add tweet component

---
+++ components/IconButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { EvilIcons } from '@expo/vector-icons'
+
+import { Text, View } from "./Themed";
+
+type IconButtonProps = {
+  icon: React.ComponentProps<typeof EvilIcons>['name']
+  text?: string | number
+}
+
+const IconButton = ({ icon, text }: IconButtonProps) => {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <EvilIcons name={icon} size={22} color="gray" />
+      <Text style={{ fontSize: 12, color: 'gray' }}>{text}</Text>
+    </View>
+  )
+}
+
+export default IconButton
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import { StyleSheet, Image } from "react-native";
+import { Entypo } from '@expo/vector-icons'
 
 import { Text, View } from "./Themed";
+import IconButton from "./IconButton";
 import { TweetType } from "../types";
 
 type TweetProps = {
@@ -12,8 +15,29 @@ export default function Tweet({ tweet }: TweetProps) {
     <View style={styles.twitContainer}>
       <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
       <View style={styles.mainContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>@{tweet.user.username} · 2h</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={16}
+            color="gray"
+            style={{ marginLeft: 'auto' }}
+          />
+        </View>
         <Text style={styles.content}>{tweet.content}</Text>
+        
+        {!!tweet.image && (
+          <Image src={tweet.image} style={styles.image} />
+        )}
+
+        <View style={styles.footer}>
+          <IconButton icon="comment" text={tweet.numberOfComments} />
+          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+          <IconButton icon="heart" text={tweet.numberOfLikes} />
+          <IconButton icon="chart" text={tweet.impressions || 0} />
+          <IconButton icon="share-apple" />
+        </View>
       </View>
     </View>
   );
@@ -39,8 +63,23 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: '500',
   },
+  username: {
+    color: 'gray',
+    marginLeft: 5,
+  },
   content: {
     lineHeight: 20,
     marginTop: 5,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 16 / 10,
+    marginVertical: 10,
+    borderRadius: 12,
+  },
+  footer: {
+    flexDirection: 'row',
+    marginVertical: 5,
+    justifyContent: 'space-between',
   },
 });
