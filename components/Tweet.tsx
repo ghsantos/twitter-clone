83fd7323c6f176b59e2c6b01ea50858a01f76d63
ghsantos <ghsantos933@gmail.com>
2023-08-28 18:52:37 -0300

    add basic tweet component

---
+++ components/Tweet.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Image } from "react-native";
+
+import { Text, View } from "./Themed";
+
+export default function Tweet({ tweet }) {
+  return (
+    <View style={styles.twitContainer}>
+      <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
+      <View style={styles.mainContainer}>
+        <Text style={styles.name}>{tweet.user.name}</Text>
+        <Text style={styles.content}>{tweet.content}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  twitContainer: {
+    flexDirection: "row",
+    width: "100%",
+    padding: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'lightgrey'
+  },
+  mainContainer: {
+    marginLeft: 15,
+    flex: 1,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  name: {
+    fontWeight: '500',
+  },
+  content: {
+    lineHeight: 20,
+    marginTop: 5,
+  },
+});
