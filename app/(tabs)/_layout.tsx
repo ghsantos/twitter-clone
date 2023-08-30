4b0a36dd54190d48a29737a2eaf2ffcba5c9c35b
ghsantos <ghsantos933@gmail.com>
2023-08-30 14:30:29 -0300

    eas build 4

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Test 1',
+          title: 'ASDF 123',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
