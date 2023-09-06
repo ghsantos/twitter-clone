86835662b337c30b0d3aa23c48f842aa9e68f7f5
ghsantos <ghsantos933@gmail.com>
2023-09-06 17:53:10 -0300

    new tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Image, FlatList } from "react-native";
+import { StyleSheet, Image, FlatList, Pressable } from "react-native";
+import { Entypo } from '@expo/vector-icons';
 
 import { Text, View } from "../../components/Themed";
 import Tweet from "../../components/Tweet";
 
 import tweets from "../../assets/data/tweets";
+import { Link } from "expo-router";
 const tweet = tweets[0];
 
 export default function TabOneScreen() {
@@ -13,6 +15,12 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
+
+      <Link href="/new-tweet" asChild>
+        <Pressable style={styles.floatingButton}>
+          <Entypo name="plus" size={24} color="white" />
+        </Pressable>
+      </Link>
     </View>
   );
 }
@@ -23,4 +31,23 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
+  floatingButton: {
+    backgroundColor: '#1D9BF0',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    right: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  }
 });
---
+++ app/new-tweet.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Image, Pressable, StyleSheet, TextInput, SafeAreaView } from "react-native";
+import { Text, View } from "../components/Themed";
+import { Link, useRouter } from "expo-router";
+
+const user = {
+  id: 'u1',
+  username: 'VadimNotJustDev',
+  name: 'Vadim',
+  image:
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+}
+
+export default function NewTweet() {
+  const router = useRouter()
+  const [text, setText] = useState('')
+
+  const onTweetPress = () => {
+    router.back()
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Link href="../" style={{ fontSize: 18 }}>Cancel</Link>
+          <Pressable onPress={onTweetPress} style={styles.button}>
+            <Text style={styles.buttonText}>Tweet</Text>
+          </Pressable>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Image source={{ uri: user.image }} style={styles.image} />
+          <TextInput
+            value={text}
+            onChangeText={setText}
+            placeholder="What's happening?"
+            multiline
+            numberOfLines={5}
+            style={{ flex: 1 }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#1D9BF0',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 50,
+    marginRight: 12,
+  }
+})
