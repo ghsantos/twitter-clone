a378df083544ffea92dffd19c3e7e409e3192806
ghsantos <ghsantos933@gmail.com>
2023-09-08 17:02:58 -0300

    added drawer navigator

---
+++ app/(drawer)/bookmarks.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Bookmarks() {
+  return (
+    <View style={styles.container}>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
---
+++ app/(drawer)/twitter-blue.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function TwitterBlue() {
+  return (
+    <View style={styles.container}>
+      <Text>Twitter Blue</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
